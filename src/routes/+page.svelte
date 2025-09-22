9889e4ad2a6ee58dae4e3716d783da3bc017b01c
Ryan Holloway <C00283423@setu.ie>
2025-09-22 12:08:31 +0100

    Base Class for Cat set up - Switch statment for Emotion based on Percentage working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- +page.svelte -->
+<script lang="ts">
+  import Cat from '$lib/components/Cat.svelte'; 
+  import { BodyAccessory, HeadAccessory, SkinColour } from '../types/Cat';
+
+  let percentage : number = 100;
+  let Head : HeadAccessory = HeadAccessory.None;
+  let Body : BodyAccessory = BodyAccessory.None;
+  let Skin : SkinColour = SkinColour.White;
+  let streak = 1;
+</script>
+
+<main>
+  <Cat {percentage} {Head} {Body} {Skin} {streak} />
+</main>
